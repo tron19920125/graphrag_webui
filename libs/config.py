6819410bb7b7ee95c4d0bf2023cfaa870f49a10b
Theodore Niu <theodore.niu@gmail.com>
2025-01-08 15:42:47 +0000

    update to 1.1.0

--- libs/config.py
+++ libs/config.py
@@ -6,7 +6,7 @@ from datetime import datetime
 load_dotenv()
 
 app_version = "0.1.2"
-graphrag_version = "1.0.1"
+graphrag_version = "1.1.0"
 
 app_started_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 app_name = "graphrag"
