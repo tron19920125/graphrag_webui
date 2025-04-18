6124d0215bbc79bcde98808b208b5f9e92e9d722
tron <bian19920125@gmail.com>
2025-04-18 01:00:17 +0000

    Update index_preview to load artifacts path from configuration and modify get_parquet_file to accept this path as a parameter.

--- cli/index_preview.py
+++ cli/index_preview.py
@@ -2,29 +2,34 @@ import os
 import pandas as pd
 from cli.logger import get_logger
 from cli.types import PreviewType
-from cli.common import project_path
+from cli.common import project_path, load_graphrag_config
 
 logger = get_logger('index_preview')
 
 def index_preview(project_name: str, type: PreviewType):
+
+    config = load_graphrag_config(project_name)
+
+    artifacts_path = config.storage.base_dir
+
     if type == PreviewType.entities.value:
-        get_parquet_file(project_name=project_name, artifact_name="create_final_entities.parquet")
+        get_parquet_file(project_name=project_name, artifact_name="create_final_entities.parquet", artifacts_path=artifacts_path)
     elif type == PreviewType.nodes.value:
-        get_parquet_file(project_name=project_name, artifact_name="create_final_nodes.parquet")
+        get_parquet_file(project_name=project_name, artifact_name="create_final_nodes.parquet", artifacts_path=artifacts_path)
     elif type == PreviewType.communities.value:
-        get_parquet_file(project_name=project_name, artifact_name="create_final_communities.parquet")
+        get_parquet_file(project_name=project_name, artifact_name="create_final_communities.parquet", artifacts_path=artifacts_path)
     elif type == PreviewType.community_reports.value:
-        get_parquet_file(project_name=project_name, artifact_name="create_final_community_reports.parquet")
+        get_parquet_file(project_name=project_name, artifact_name="create_final_community_reports.parquet", artifacts_path=artifacts_path)
     elif type == PreviewType.documents.value:
-        get_parquet_file(project_name=project_name, artifact_name="create_final_documents.parquet")
+        get_parquet_file(project_name=project_name, artifact_name="create_final_documents.parquet", artifacts_path=artifacts_path)
     elif type == PreviewType.relationships.value:
-        get_parquet_file(project_name=project_name, artifact_name="create_final_relationships.parquet")
+        get_parquet_file(project_name=project_name, artifact_name="create_final_relationships.parquet", artifacts_path=artifacts_path)
     elif type == PreviewType.text_units.value:
-        get_parquet_file(project_name=project_name, artifact_name="create_final_text_units.parquet")
+        get_parquet_file(project_name=project_name, artifact_name="create_final_text_units.parquet", artifacts_path=artifacts_path)
 
 
-def get_parquet_file(project_name:str, artifact_name: str):
-    parquet_path = f"{project_path(project_name)}/output/{artifact_name}"
+def get_parquet_file(project_name:str, artifact_name: str, artifacts_path: str):
+    parquet_path = f"{artifacts_path}/{artifact_name}"
     
     if not os.path.exists(parquet_path):
         logger.error(f"File not found: `{artifact_name}`")
